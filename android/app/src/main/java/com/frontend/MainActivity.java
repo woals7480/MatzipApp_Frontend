a23d00c5950d5b6adccdb872f9b7e9916fb78637
woals7480 <94383932+woals7480@users.noreply.github.com>
2024-09-23 00:14:31 +0900

    react-navigation

--- android/app/src/main/java/com/frontend/MainActivity.java
+++ android/app/src/main/java/com/frontend/MainActivity.java
@@ -1,12 +1,12 @@
 package com.frontend;
 
 import com.facebook.react.ReactActivity;
+import android.os.Bundle;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
 public class MainActivity extends ReactActivity {
-
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
@@ -28,5 +28,10 @@ public class MainActivity extends ReactActivity {
         getMainComponentName(),
         // If you opted-in for the New Architecture, we enable the Fabric Renderer.
         DefaultNewArchitectureEntryPoint.getFabricEnabled());
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 }
